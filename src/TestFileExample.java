e5a5463cb32e6209be0a851fc0e5c2713dd7e678
MariuszMachnicki <mariusz.machnicki@testarmy.com>
2020-07-23 12:44:29 +0200

    update

--- src/TestFileExample.java
+++ src/TestFileExample.java
@@ -1,3 +1,24 @@
+
 public class TestFileExample {
+
+    static void myMethod() {
+        int x = 4;
+        String message = "Twoja liczba ";
+
+        System.out.println(message + x);
+    }
+
+}
+    static void myMethod2(String fname) {
+        int liczba = 4;
+        String message = "Twoja liczba ";
+        System.out.println(fname + "!" + message + liczba);
+
+
+    public static void main(String[] args) {
+        myMethod();
+        myMethod2("Mario");
+ }
+
 }
 
